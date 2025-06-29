6eaec632f09d7f26a5b55db415e9809c240c1a8c
Alaa Zamel <alaa.zamel23@gmail.com>
2025-06-29 21:27:29 +0300

    complex parameter expressions implementation

--- university/src/main/java/com/example/university/repository/StudentRepository.java
+++ university/src/main/java/com/example/university/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.university.repository;
 
 import com.example.university.domain.Student;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
@@ -11,4 +12,16 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
   List<Student> findByAge(int age);
 
   List<Student> findByAttendeeLastName(String lastName);
+
+  List<Student> findByAttendeeFirstNameAndAttendeeLastName(String firstName, String lastName);
+
+  Optional<Student> findTop1ByOrderByAgeDesc();
+
+  List<Student> findByAgeLessThan(int age);
+
+  List<Student> findByAttendeeLastNameLike(String lastName);
+
+  Optional<Student> findFirstByOrderByAttendeeLastNameAsc();
+
+  List<Student> findTop3ByOrderByAgeDesc();
 }
--- university/src/test/java/com/example/university/dao/FindByClausesAndExpressions.java
+++ university/src/test/java/com/example/university/dao/FindByClausesAndExpressions.java
@@ -3,14 +3,13 @@ package com.example.university.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.example.university.business.UniversityService;
+import com.example.university.domain.Student;
+import com.example.university.repository.StudentRepository;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.university.business.UniversityService;
-import com.example.university.domain.Student;
 
 /**
  * Test queries that involve clauses and expressions
@@ -18,33 +17,44 @@ import com.example.university.domain.Student;
 @SpringBootTest
 public class FindByClausesAndExpressions {
 
-    @Autowired
-    private UniversityService universityService;
+  @Autowired
+  private UniversityService universityService;
 
-    @Autowired
-    private StudentDao studentDao;
+  @Autowired
+  private StudentRepository studentRepository;
 
-    @Test
-    public void findByClausesAndExpressions() {
-        // Test Create
-        UniversityFactory.fillUniversity(universityService);
-        List<Student> students = universityService.findAllStudents();
-        Student firstStudent = students.get(0);
+  @Test
+  public void findByClausesAndExpressions() {
 
-        assertTrue(studentDao.findOldest().get().getAge() == 22);
+    // Test Create
+    UniversityFactory.fillUniversity(universityService);
+    List<Student> students = universityService.findAllStudents();
+    Student firstStudent = students.getFirst();
 
-        assertEquals(firstStudent, studentDao.findByFirstAndLastName(firstStudent.getAttendee().getFirstName(),
-                firstStudent.getAttendee().getLastName()).get(0));
+    assertEquals(22, (int) studentRepository.findTop1ByOrderByAgeDesc().get().getAge());
 
-        studentDao.findByAgeLessThan(20).stream().forEach(s-> assertTrue(s.getAge() < 20));
+    assertEquals(
+        firstStudent,
+        studentRepository.findByAttendeeFirstNameAndAttendeeLastName(
+            firstStudent.getAttendee().getFirstName(),
+            firstStudent.getAttendee().getLastName()
+        ).getFirst()
+    );
 
-        studentDao.findSimilarLastName("%o%")
-                .stream().forEach(s->assertTrue(s.getAttendee().getLastName().contains("o")));
+    studentRepository.findByAgeLessThan(20).forEach(s -> assertTrue(s.getAge() < 20));
 
-        assertTrue(studentDao.findFirstInAlphabet().get().getAttendee().getLastName().equals("Doe"));
+    studentRepository.findByAttendeeLastNameLike("%o%")
+        .forEach(s -> assertTrue(s.getAttendee().getLastName().contains("o")));
 
-        List<Student> students3 = studentDao.find3Oldest();
-        assertTrue(students3.size() == 3);
-        students3.stream().forEach(s -> assertTrue(s.getAge() != 18));
-    }
+    assertEquals("Doe",
+        studentRepository.findFirstByOrderByAttendeeLastNameAsc()
+            .get()
+            .getAttendee()
+            .getLastName()
+    );
+
+    List<Student> students3 = studentRepository.findTop3ByOrderByAgeDesc();
+    assertEquals(3, students3.size());
+    students3.forEach(s -> assertTrue(s.getAge() != 18));
+  }
 }
